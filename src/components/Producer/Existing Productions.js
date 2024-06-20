8417332a015a2df78a3644b57198f6a4dfb1e93a
Kalpanaa <aeruvakalpana@gmail.com>
2024-06-20 23:14:45 +0530

    changes with alignment

--- src/components/Producer/Existing Productions.js
+++ src/components/Producer/Existing Productions.js
@@ -1,6 +1,6 @@
-import React, {useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
-import {serverURL} from "../../constants"
+import { serverURL } from "../../constants"
 import axios from "axios";
 // import b from './barbie.jpg';
 // import o from './oh1.jpg';
@@ -15,9 +15,9 @@ const flexColumn = {
   display: "flex",
   flexDirection: "column",
 };
-const flexRow={
+const flexRow = {
   display: "flex",
-  flexDirection: "row", 
+  flexDirection: "row",
 }
 const borderBox = {
   boxSizing: "border-box",
@@ -57,20 +57,20 @@ const useStyles = makeStyles((theme) => ({
     padding: "1rem",
     overflow: "auto",
     backgroundColor: "#d8e8ee",
-    flex:"1",
+    flex: "1",
   },
-  row:{
+  row: {
     ...flexRow,
-    justifyContent:"space-around",
+    justifyContent: "space-around",
   },
-  Tile:{
+  Tile: {
     display: "flex",
     flexWrap: "wrap",
     marginLeft: "30%",
     gap: "40px",
     rowGap: "30px",
   },
-  tile:{
+  tile: {
     bordeRradius: "5px 5px",
     width: "200px",
     height: "300px",
@@ -80,75 +80,78 @@ const useStyles = makeStyles((theme) => ({
     alignItems: "center",
     justifyContent: "center",
     overflow: "hidden",
-    borderRadius:"5px 5px",
+    borderRadius: "5px 5px",
   },
-  tileimg:{
+  tileimg: {
     width: "100%",
-    height:"100%",
+    height: "100%",
     objectFit: "cover",
   }
 }));
-function ExistingProds(){
-    const classes = useStyles();
-    const navigate=useNavigate();
-    const [loading, setLoading] = useState(true);
-    const [finaldata,setfinaldata]=useState();
-    const handleclick=()=>{
-      navigate("/Producer/AddProduction");
-    }
-    const handleClickprod_crew=()=>{
-      navigate("/Prod_crew")
-    }
-    useEffect(()=>{
-      let isCancelled = false;
+function ExistingProds() {
+  const classes = useStyles();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+  const [finaldata, setfinaldata] = useState();
+  const handleclick = () => {
+    navigate("/Producer/AddProduction");
+  }
+  const handleClickprod_crew = () => {
+    navigate("/Prod_crew")
+  }
+
+  useEffect(() => {
+    let isCancelled = false;
     if (isCancelled === false) setLoading(true);
     axios({
       method: "GET",
       url: `${serverURL}/api/production_list`,
       headers: {
-        Authorization: "Bearer " +"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEiLCJQcm9kdWN0aW9uX2lkIjoiMyIsImxvZ2luX3R5cGUiOiJBZG1pbiJ9.ekUr9ZiKEODQFqLOSTM1XTDqkLiq3YQgcxtlDjgin3c",
-    }})
-    .then((response) => {
-      const finalresponse=response.data;
-      console.log(finalresponse);
-      setfinaldata(finalresponse.result[0])
-      setLoading(false);
+        Authorization: "Bearer " + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEiLCJQcm9kdWN0aW9uX2lkIjoiMyIsImxvZ2luX3R5cGUiOiJBZG1pbiJ9.ekUr9ZiKEODQFqLOSTM1XTDqkLiq3YQgcxtlDjgin3c",
+      }
     })
-    .catch((error) => {
-      console.error('Error fetching production list:', error);
-      setLoading(false);
-    });
-}, []);
-if (loading) {
-  return <div>Loading...</div>;
-}
-    return(
-        <>
-        <div className={classes.containerexist}>
+      .then((response) => {
+        const finalresponse = response.data;
+        console.log(finalresponse);
+        setfinaldata(finalresponse.result[0])
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching production list:', error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <>
+      <div className={classes.containerexist}>
         <div className={classes.containerBodyexist}>
-            <Card className={classes.existForm}>
+          <Card className={classes.existForm}>
             <CardContent>
-            <Paper style={{width:"100%", textAlign:"center"}}><h2>Exsisting Productions</h2></Paper>
-                <Card>
-                    <CardContent>
-        <div className={classes.row}>
+              <Paper style={{ width: "100%", textAlign: "center" }}><h2>Exsisting Productions</h2></Paper>
+              <Card>
+                <CardContent>
+                  <div className={classes.row}>
+                  </div>
+                  <div className={classes.Tile}>
+                    <div style={{ display: "flex", flexDirection: "column" }}>
+                      <div className={classes.tile}>
+                        {finaldata && <img src={finaldata.Image_path} alt={finaldata.Production_Name} onClick={handleClickprod_crew} style={{ cursor: "pointer" }} />}
+                      </div>
+                      {finaldata && <label style={{ marginTop: "5px", marginLeft: "20%" }}>{finaldata.Production_Name}</label>}
+                    </div>
+                  </div>
+                  <Button variant='contained' color='primary' onClick={handleclick} style={{ float: "right", marginBottom: "10px" }}>Add New Production</Button>
+                </CardContent>
+              </Card>
+            </CardContent>
+          </Card>
         </div>
-        <div className={classes.Tile}>
-        <div style={{display:"flex",flexDirection:"column" }}>
-            <div className={classes.tile}>
-            {finaldata && <img src={finaldata.Image_path} alt={finaldata.Production_Name} onClick={handleClickprod_crew} style={{cursor:"pointer"}}/>}
-            </div>
-            {finaldata && <label style={{marginTop:"5px",marginLeft:"20%"}}>{finaldata.Production_Name}</label>}
-        </div>
-        </div>
-        <Button variant='contained' color='primary' onClick={handleclick} style={{float:"right",marginBottom:"10px"}}>Add New Production</Button>
-        </CardContent>
-        </Card>
-        </CardContent>
-        </Card>
-        </div>
-        </div>
-        </>
-    )
+      </div>
+    </>
+  )
 }
 export default ExistingProds;
